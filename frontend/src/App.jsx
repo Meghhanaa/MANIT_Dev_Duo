75d660258ae0e8b34e6dc1304d1a06323a57c109
Meghana Tamrakar <tamrakarmeghana@gmail.com>
2025-03-08 17:16:00 +0530

    backend_1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Home from './component/Home'
+import Login from './component/Login'
 // import Sign from './component/Sign'
 
 function App() {
 
   return (
     <>
+
+      <Login></Login>
       <Home></Home>
     </>
   )
